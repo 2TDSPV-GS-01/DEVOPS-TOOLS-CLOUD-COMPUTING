d3ab145a51e34c3d9cd0910575fafe57b98c8cba
Francesco Monteiro Di Benedetto <161745078+FrancescoDiBenedetto@users.noreply.github.com>
2025-05-29 11:30:09 -0300

    Criar a VM

---
+++ create_vm.sh
@@ -0,0 +1,46 @@
+# Comando para se criar um grupo de recussos
+az group create --name rg-FonteViva --location brazilsouth --tags environment-FonteViva
+
+# AlmaLinux é leve, compatível com Red Hat e gratuito
+# Mesmo sistema que nossa VM FREE da aula, podemos subir os testes nela
+az vm create \
+    --name vm-lnx-FonteViva \
+    --resource-group rg-FonteViva \
+    --accelerated-networking false \
+    --accept-term \
+    --additional-events false \
+    --admin-password FonteViva@2tdsvms \
+    --admin-username admfonteviva \
+    --authentication-type password \
+    --computer-name FonteViva-01 \
+    --enable-agent true \
+    --enable-auto-update true \
+    --enable-hibernation false \
+    --enable-redeploy true \
+    --image almalinux:almalinux-x86_64:9-gen2:latest \
+    --location brazilsouth \
+    --nic-delete-option Delete \
+    --nsg-rule SSH \
+    --os-disk-delete-option Delete \
+    --os-disk-name FonteViva-disk \
+    --patch-mode ImageDefault \
+    --priority Spot \
+    --size Standard_B2als_v2 \
+    --tags environment-FonteViva \
+    --ultra-ssd-enabled false \
+    --zone 1
+
+# Abrir as portas necessarias
+
+az vm open-port --port 80 -g rg-FonteViva \
+    -n vm-lnx-FonteViva --priority 1100
+
+az vm open-port --port 1521 -g rg-FonteViva \
+    -n vm-lnx-FonteViva --priority 1200
+
+az vm open-port --port 8080 -g rg-FonteViva \
+    -n vm-lnx-FonteViva --priority 1300
+
+# desligar de forma automatica a VM
+az vm auto-shutdown -g rg-FonteViva -n vm-lnx-FonteViva --time 2359 --email "francescomdibe@gmail.com"
+
